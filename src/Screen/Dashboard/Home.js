d7b58fc9f2a41fa4a6267dba72cf372f13052b62
Maulik Virani <maulik.virani@thinkbar.in>
2023-07-17 14:09:23 +0530

    gradient added in spotlight

--- src/Screen/Dashboard/Home.js
+++ src/Screen/Dashboard/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useReducer, createRef, useRef} from 'react';
+import React, { useEffect, useState, useReducer, createRef, useRef } from 'react';
 import {
   Image,
   ImageBackground,
@@ -29,18 +29,18 @@ import {
 } from 'react-native-responsive-screen';
 import Header from '../../Components/Header';
 import ImagePath from '../../assets/ImagePath';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {COLORS, FONTS} from '../../Components/constants';
-import {useDispatch, useSelector} from 'react-redux';
-import {spotLightApi} from '../../redux/reducers/spotLightSlice';
-import {artistApi} from '../../redux/reducers/artistSlice';
-import {LocationApi} from '../../redux/reducers/clubLocationSlice';
-import {upComingEventApi} from '../../redux/reducers/upComingEventSlice';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { COLORS, FONTS } from '../../Components/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { spotLightApi } from '../../redux/reducers/spotLightSlice';
+import { artistApi } from '../../redux/reducers/artistSlice';
+import { LocationApi } from '../../redux/reducers/clubLocationSlice';
+import { upComingEventApi } from '../../redux/reducers/upComingEventSlice';
 import ApiCall from '../../redux/CommanApi';
-import {ARTIST} from '../../services/Apis';
+import { ARTIST } from '../../services/Apis';
 import Disclamer from '../../Components/Disclamer';
 import CustomButton from '../../Components/TextInput_And_Button/CustomButton';
-import {addCoordinates} from '../../redux/reducers/clubLocationSlice';
+import { addCoordinates } from '../../redux/reducers/clubLocationSlice';
 import Geolocation from '@react-native-community/geolocation';
 // import Geolocation from 'react-native-geolocation-service';
 import CitySelector from '../../Components/CitySelector';
@@ -49,12 +49,13 @@ import {
   getBottomSpace,
   getStatusBarHeight,
 } from 'react-native-iphone-screen-helper';
-import {showLoader} from '../../redux/reducers/loaderSlice';
+import { showLoader } from '../../redux/reducers/loaderSlice';
 import Toast from 'react-native-simple-toast';
 import moment from 'moment';
-import {logEvent} from '../../utils/AddFirebaseEvent';
-import {currentCity} from '../../redux/reducers/citySelectorSlice';
-import {getData} from '../../Components/Helper';
+import { logEvent } from '../../utils/AddFirebaseEvent';
+import { currentCity } from '../../redux/reducers/citySelectorSlice';
+import { getData } from '../../Components/Helper';
+import LinearGradient from 'react-native-linear-gradient';
 
 const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
@@ -169,7 +170,7 @@ const Home = props => {
           <ActivityIndicator
             color={'#fff'}
             size={'large'}
-            style={{marginLeft: 8}}
+            style={{ marginLeft: 8 }}
           />
         ) : null}
       </View>
@@ -213,7 +214,7 @@ const Home = props => {
       error => {
         console.log('location error', error.code, error.message);
       },
-      {enableHighAccuracy: true, timeout: 500000},
+      { enableHighAccuracy: true, timeout: 500000 },
     );
   };
 
@@ -262,11 +263,11 @@ const Home = props => {
     }
   };
   const Tabs = [
-    {mapIcon: ImagePath.listTwoImg, title: 'Cocktail Bar'},
-    {mapIcon: ImagePath.clubLocation, title: 'Nightclub'},
-    {mapIcon: ImagePath.listTwoImg, title: 'Cocktail'},
-    {mapIcon: ImagePath.clubLocation, title: 'Nightclub'},
-    {mapIcon: ImagePath.listTwoImg, title: 'Cocktail'},
+    { mapIcon: ImagePath.listTwoImg, title: 'Cocktail Bar' },
+    { mapIcon: ImagePath.clubLocation, title: 'Nightclub' },
+    { mapIcon: ImagePath.listTwoImg, title: 'Cocktail' },
+    { mapIcon: ImagePath.clubLocation, title: 'Nightclub' },
+    { mapIcon: ImagePath.listTwoImg, title: 'Cocktail' },
   ];
   const [clubsSpotlight, setClubsSpotlight] = useState([]);
   const [artistsSpotlight, setArtistsSpotlight] = useState([]);
@@ -317,7 +318,7 @@ const Home = props => {
           <ActivityIndicator
             color={'#fff'}
             size={'large'}
-            style={{marginLeft: 8}}
+            style={{ marginLeft: 8 }}
           />
         ) : null}
       </View>
@@ -341,13 +342,13 @@ const Home = props => {
     }
   };
   const [artistData, setArtistData] = useState([
-    {Bar_Icon: ImagePath.listImg},
-    {Bar_Icon: ImagePath.artistImg},
-    {Bar_Icon: ImagePath.artistImg1},
-    {Bar_Icon: ImagePath.listImg},
-    {Bar_Icon: ImagePath.artistImg},
+    { Bar_Icon: ImagePath.listImg },
+    { Bar_Icon: ImagePath.artistImg },
+    { Bar_Icon: ImagePath.artistImg1 },
+    { Bar_Icon: ImagePath.listImg },
+    { Bar_Icon: ImagePath.artistImg },
   ]);
-  const artistRenderItem = ({item, index}) => (
+  const artistRenderItem = ({ item, index }) => (
     <TouchableOpacity
       onPress={() => {
         logEvent('artist_detail', item?.name);
@@ -355,7 +356,7 @@ const Home = props => {
           artistListDetail: item,
         });
       }}
-      style={{marginTop: 20, height: wp(28)}}>
+      style={{ marginTop: 20, height: wp(28) }}>
       <FastImage
         style={{
           height: wp(28),
@@ -364,7 +365,7 @@ const Home = props => {
           marginLeft: 15,
           resizeMode: 'cover',
         }}
-        source={{uri: item?.images[0] ?? ''}}
+        source={{ uri: item?.images[0] ?? '' }}
       />
       <Text
         style={[
@@ -425,7 +426,7 @@ const Home = props => {
     }
   };
 
-  const UpcomingData_RenderItem = ({item, index}) => {
+  const UpcomingData_RenderItem = ({ item, index }) => {
     return (
       <TouchableOpacity
         onPress={() => {
@@ -435,8 +436,8 @@ const Home = props => {
           });
         }}>
         {item?.images?.length &&
-        item?.images[0] &&
-        typeof item?.images[0]?.path == 'string' ? (
+          item?.images[0] &&
+          typeof item?.images[0]?.path == 'string' ? (
           <FastImage
             style={{
               height: hp(20),
@@ -444,7 +445,7 @@ const Home = props => {
               resizeMode: 'cover',
               borderRadius: 10,
             }}
-            source={{uri: item?.images[0]?.path}}
+            source={{ uri: item?.images[0]?.path }}
           />
         ) : (
           <View
@@ -488,7 +489,7 @@ const Home = props => {
             {moment(item?.eventDate).format('MMM')}
           </Text>
         </View>
-        <View style={{position: 'absolute', left: 9, bottom: 9}}>
+        <View style={{ position: 'absolute', left: 9, bottom: 9 }}>
           {/* <TouchableOpacity
             style={{
               borderRadius: 10,
@@ -523,8 +524,8 @@ const Home = props => {
               marginVertical: 5,
             }}>
             {item?.artists?.length &&
-            item?.artists[0]?.images?.length &&
-            item?.artists[0]?.images[0] ? (
+              item?.artists[0]?.images?.length &&
+              item?.artists[0]?.images[0] ? (
               <Image
                 style={{
                   height: 20,
@@ -533,7 +534,7 @@ const Home = props => {
                   resizeMode: 'contain',
                   marginRight: 6,
                 }}
-                source={{uri: item?.artists[0]?.images[0]}}
+                source={{ uri: item?.artists[0]?.images[0] }}
               />
             ) : null}
             <Text
@@ -557,7 +558,7 @@ const Home = props => {
               item?.eventEndTime,
             ).format('hh:mm A')}`}
           </Text>
-          <View style={{flexDirection: 'row'}}>
+          <View style={{ flexDirection: 'row' }}>
             <Image
               style={{
                 height: 10,
@@ -598,7 +599,7 @@ const Home = props => {
           <ActivityIndicator
             color={'#fff'}
             size={'large'}
-            style={{marginLeft: 8}}
+            style={{ marginLeft: 8 }}
           />
         ) : null}
       </View>
@@ -618,7 +619,7 @@ const Home = props => {
           <ActivityIndicator
             color={'#fff'}
             size={'large'}
-            style={{marginLeft: 8}}
+            style={{ marginLeft: 8 }}
           />
         ) : null}
       </View>
@@ -661,16 +662,18 @@ const Home = props => {
       Location: 'Sector 52, Near Ahuja Tower',
     },
   ]);
-  const SpotlightData_RenderItem = ({item, index}) => {
+  const SpotlightData_RenderItem = ({ item, index }) => {
     return (
       <TouchableOpacity
         onPress={() => {
-          props.navigation.push('ClubDetails', {listDetail: item});
+          props.navigation.push('ClubDetails', { listDetail: item });
         }}
         style={{
           marginLeft: wp(2.5),
           marginRight: index == 0 ? 15 : 15,
           marginLeft: index == 0 ? 15 : 0,
+          overflow: 'hidden',
+          borderRadius: 10,
         }}>
         {item?.media?.ambienceImages?.length ? (
           <FastImage
@@ -680,7 +683,7 @@ const Home = props => {
               resizeMode: 'cover',
               borderRadius: 10,
             }}
-            source={{uri: item?.media?.ambienceImages[0] || ''}}
+            source={{ uri: item?.media?.ambienceImages[0] || '' }}
           />
         ) : (
           <View
@@ -693,7 +696,16 @@ const Home = props => {
             }}
           />
         )}
-        <View style={{position: 'absolute', left: 15, bottom: 15}}>
+        <LinearGradient
+          colors={['transparent', '#0d0d0d']}
+          style={{
+            width: '100%',
+            position: 'absolute',
+            paddingLeft: 15,
+            paddingBottom: 15,
+            bottom: 0,
+            overflow: 'hidden',
+          }}>
           <Text
             style={{
               fontSize: 20,
@@ -723,7 +735,7 @@ const Home = props => {
             ]}>
             {item.locality}, {item.city}
           </Text>
-        </View>
+        </LinearGradient>
       </TouchableOpacity>
     );
   };
@@ -731,8 +743,7 @@ const Home = props => {
     console.log('locationdata ---', locationLatLong);
     try {
       ApiCall(
-        `api/nearby-clubs?coordinates=${locationLatLong?.latitude || ''}${
-          locationLatLong?.latitude ? ',' : ''
+        `api/nearby-clubs?coordinates=${locationLatLong?.latitude || ''}${locationLatLong?.latitude ? ',' : ''
         }${locationLatLong?.longitude || ''}&radius=5000&sort_dir=desc`, //${19.136326},${72.82766}
         'GET',
       ).then(res => {
@@ -746,11 +757,11 @@ const Home = props => {
     }
   };
   const [clubsNearby, setClubNearby] = useState([]);
-  const ClubNarDatarenderItem = ({item, index}) => {
+  const ClubNarDatarenderItem = ({ item, index }) => {
     return (
       <TouchableOpacity
         onPress={() => {
-          props.navigation.push('ClubDetails', {listDetail: item});
+          props.navigation.push('ClubDetails', { listDetail: item });
         }}
         style={{
           marginLeft: index == 0 ? 15 : 0,
@@ -765,7 +776,7 @@ const Home = props => {
               resizeMode: 'cover',
               borderRadius: 10,
             }}
-            source={{uri: item?.media?.ambienceImages[0]}}
+            source={{ uri: item?.media?.ambienceImages[0] }}
           />
         ) : (
           <View
@@ -777,7 +788,7 @@ const Home = props => {
             }}
           />
         )}
-        <View style={{position: 'absolute', left: 15, bottom: 15}}>
+        <View style={{ position: 'absolute', left: 15, bottom: 15 }}>
           <Text
             style={{
               fontSize: 16,
@@ -814,11 +825,11 @@ const Home = props => {
   };
 
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <ImageBackground
         source={ImagePath.Azzir_Bg}
         resizeMode="cover"
-        style={{height: '100%'}}>
+        style={{ height: '100%' }}>
         {/* <View style={{marginHorizontal: 15, marginTop: 46, marginBottom: 14}}>
           <Header
             Back_Arrow={ImagePath.manueIcon}
@@ -875,7 +886,7 @@ const Home = props => {
             showsHorizontalScrollIndicator={false}
             // ListFooterComponent={spotLightrenderFooter}
             // onEndReachedThreshold={0.7}
-            contentContainerStyle={{marginTop: 20, zIndex: 99, paddingStart: 5}}
+            contentContainerStyle={{ marginTop: 20, zIndex: 99, paddingStart: 5 }}
             // onMomentumScrollBegin={() => {
             //   setonEndReachedCalledDuringspotLight(false);
             // }}
@@ -903,7 +914,7 @@ const Home = props => {
             renderItem={ClubNarDatarenderItem}
             showsHorizontalScrollIndicator={false}
             // ListFooterComponent={renderFooter}
-            style={{marginTop: 20, marginBottom: -hp(2), paddingStart: 5}}
+            style={{ marginTop: 20, marginBottom: -hp(2), paddingStart: 5 }}
             // onEndReachedThreshold={0.7}
             // onMomentumScrollBegin={() => {
             //   setonEndReachedCalledDuringMomentum(false);
@@ -926,7 +937,7 @@ const Home = props => {
                       <Text
                         style={[
                           styles.titleText1,
-                          {marginHorizontal: 30, textAlign: 'center'},
+                          { marginHorizontal: 30, textAlign: 'center' },
                         ]}>
                         AZZIR needs to detect you location to show the clubs
                         near you.
@@ -975,14 +986,14 @@ const Home = props => {
             horizontal={true}
             data={artistsSpotlight}
             renderItem={artistRenderItem}
-            contentContainerStyle={{paddingStart: 5, paddingEnd: 10}}
+            contentContainerStyle={{ paddingStart: 5, paddingEnd: 10 }}
             showsHorizontalScrollIndicator={false}
-            // ListFooterComponent={artistRenderFooter}
-            // onEndReachedThreshold={0.7}
-            // onMomentumScrollBegin={() => {
-            //   setonEndReachedCalledDuringArtist(false);
-            // }}
-            // onEndReached={fetchArtistData}
+          // ListFooterComponent={artistRenderFooter}
+          // onEndReachedThreshold={0.7}
+          // onMomentumScrollBegin={() => {
+          //   setonEndReachedCalledDuringArtist(false);
+          // }}
+          // onEndReached={fetchArtistData}
           />
           {/* <TouchableOpacity
             style={[
@@ -1043,9 +1054,9 @@ const Home = props => {
                 setonEndReachedCalledDuringUpcoming(false);
               }}
               ItemSeparatorComponent={() => {
-                return <View style={{width: 15}} />;
+                return <View style={{ width: 15 }} />;
               }}
-              // onEndReached={fetchUpcomingData}
+            // onEndReached={fetchUpcomingData}
             />
           ) : (
             <View
@@ -1059,7 +1070,7 @@ const Home = props => {
               <Text
                 style={[
                   styles.titleText1,
-                  {textAlign: 'center', lineHeight: 20},
+                  { textAlign: 'center', lineHeight: 20 },
                 ]}>
                 No Upcoming Events.{'\n'}Please check back later.
               </Text>
@@ -1107,7 +1118,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  hedingImg: {width: '30%', resizeMode: 'contain'},
+  hedingImg: { width: '30%', resizeMode: 'contain' },
   cardText: {
     fontFamily: FONTS.AxiformaBold,
     fontSize: 16,
@@ -1143,13 +1154,13 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
     elevation: 5,
   },
-  redioText: {color: COLORS.black},
+  redioText: { color: COLORS.black },
   textModal: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginVertical: hp(2),
   },
-  redioImg: {height: 15, width: 15, tintColor: COLORS.black},
+  redioImg: { height: 15, width: 15, tintColor: COLORS.black },
 
   button: {
     height: 60,
